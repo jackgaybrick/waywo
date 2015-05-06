402ede79eacfdd1945636ec5b18a8fe32680983e
John Gaybrick <jack.gaybrick@gmail.com>
2015-05-06 15:36:16 -0400

    added forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :age, :weight, :goal)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
+  resources :users
   resources :routines, only: [:create, :show]
   resources :exercises, only: [:show]
   resources :likes, only: [:index]
